74599ece09137ec1b38fb026977678219002ed0b
neolectron <neolectron@users.noreply.github.com>
2017-02-09 01:28:15 +0100

    first commit
    v0.1

---
+++ ripple.js
@@ -0,0 +1,27 @@
+(function()
+{
+    document.querySelectorAll('.ripple').forEach(function(el)
+    {
+        el.addEventListener('click',function(ev)
+        {
+            var offset = this.getBoundingClientRect();
+            var style  = window.getComputedStyle(this);
+            
+            var div = document.createElement('div');
+            div.classList.add('effect-ripple');
+            div.style.height        = style.height;
+            div.style.width         = style.height;
+            div.style.top           = (ev.clientY - offset.top)  - (parseInt(div.style.height) / 2);
+            div.style.left          = (ev.clientX - offset.left) -  (parseInt(div.style.width) / 2);
+            div.style.background    = style.color;
+
+            this.appendChild(div);
+
+            window.setTimeout(function()
+            {
+               div.outerHTML = "";
+               delete div;
+            },1800);
+        });
+    });
+})();
